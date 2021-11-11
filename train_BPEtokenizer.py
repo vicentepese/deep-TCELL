25b9befd48f9f273203d5fbe7a9f456491e3a737
vicentepese <sempereperisvicente@gmail.com>
2021-11-11 11:03:21 +0100

    Revert "add trainer"
    This reverts commit dde2338f6ce177fca466713c15bcec0639b64d2e.

--- train_BPEtokenizer.py
+++ train_BPEtokenizer.py
@@ -72,8 +72,8 @@ def tokenization_pipeline(settings:dict) -> None:
     trainer = BpeTrainer(min_frequency=10,
                          special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
     # Train on data 
-    tokenizer.train(files=[settings["file"]["BPEtokenizer_data"]], trainer=trainer)
-    tokenizer.save(settings["tokenizer"]["BPE"])
+    tokenizer.train(files=[settings["file"]["BPEtokenizer_data"]])
+    tokenizer.save(settings["tokenizer"]["BPE"], trainder=trainer)
     
     return tokenizer
     
