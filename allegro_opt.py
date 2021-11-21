782761b6071b4780f8395a2f867dc23f224af498
Vicente Peris Sempere <vipese@sh02-ln04.stanford.edu>
2021-11-21 02:16:14 -0800

    allegro_opt nonfunc

--- allegro_opt.py
+++ allegro_opt.py
@@ -4,12 +4,38 @@ from clearml.automation.optuna import OptimizerOptuna
 
 from clearml import Task 
 
+def job_complete_callback(
+    job_id,                 # type: str
+    objective_value,        # type: float
+    objective_iteration,    # type: int
+    job_parameters,         # type: dict
+    top_performance_job_id  # type: str
+):
+    print('Job completed!', job_id, objective_value, objective_iteration, job_parameters)
+    if job_id == top_performance_job_id:
+        print('WOOT WOOT we broke the record! Objective reached {}'.format(objective_value))
+
 def main():
+    
     task = Task.init(project_name='Hyper-Parameter Search', 
-                    task_name='Hyper-Parameter Optimization')
+                    task_name='Hyper-Parameter Optimization',                    
+                    task_type=Task.TaskTypes.optimizer,
+                    reuse_last_task_id=False)
+    
+    # experiment template to optimize in the hyper-parameter optimization
+    args = {
+        'template_task_id': None,
+        'run_as_service': False,
+    }
+    args = task.connect(args)
+    
+    # Get the template task experiment that we want to optimize
+    if not args['template_task_id']:
+        args['template_task_id'] = Task.get_task(
+            project_name='protein_binding', task_name='deep-TCELL').id
 
     optimizer = HyperParameterOptimizer(
-        base_task_id="a020ed9f85564708aff0e91cf55f9835",  
+        base_task_id=args['template_task_id'],  
         # setting the hyper-parameters to optimize
             hyper_parameters=[
                 UniformIntegerParameterRange('batch_size', min_value=8, max_value=32, step_size=8),
@@ -19,7 +45,7 @@ def main():
             
         # setting the objective metric we want to maximize/minimize
             objective_metric_title='Loss',
-            objective_metric_series='test:',
+            objective_metric_series='test',
             objective_metric_sign='min',  
             
         # setting optimizer 
@@ -34,10 +60,15 @@ def main():
             max_iteration_per_job=150000)
 
 
-    optimizer.set_report_period(1) # setting the time gap between two consecutive reports
-    optimizer.start()
+    optimizer.set_report_period(0.2) # setting the time gap between two consecutive reports
+    optimizer.start(job_complete_callback=job_complete_callback)
     optimizer.wait() # wait until process is done
-    optimizer.stop() # make sure background optimization stopped
+    top_exp = optimizer.get_top_experiments(top_k=3)
+    print([t.id for t in top_exp])
+    # make sure background optimization stopped
+    optimizer.stop()
+    
+    print('We are done, good bye')
 
 if __name__ == "__main__":
     main()
--- slurm/opt_batch.sh
+++ slurm/opt_batch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=OPT_ALLEGRO.out
 #SBATCH --error=OPT_ALLEGRO.err
 #SBATCH -p gpu
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 #SBATCH -G 5
 
 # Submit
