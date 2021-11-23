d29d0e867b2bfc1558d0e8f91eff5b8a6673c6c7
Vicente Peris Sempere <vipese@sh03-ln04.stanford.edu>
2021-11-23 03:10:27 -0800

    requirement + arg in base task id

--- allegro_opt.py
+++ allegro_opt.py
@@ -35,7 +35,7 @@ def main():
             project_name='protein_binding', task_name='deep-TCELL').id
 
     optimizer = HyperParameterOptimizer(
-        base_task_id="b0c204b3865d4ff0a0fed6cc02a3c0b1",  
+        base_task_id=args['template_task_id'],  
         
         # setting the hyper-parameters to optimize
             hyper_parameters=[
