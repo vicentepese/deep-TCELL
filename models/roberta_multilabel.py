82fae9a4786ec2d20de05d07b80e4b67712f452d
vicentepese <sempereperisvicente@gmail.com>
2022-05-25 11:02:56 +0200

    switched to lighnint, overriden functions

--- models/roberta_multilabel.py
+++ models/roberta_multilabel.py
@@ -1,10 +1,16 @@
+from syslog import LOG_EMERG
+import sklearn.metrics as metrics
 import torch
+import torchmetrics
 import transformers
+
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
 
 from torch import nn, tensor
 from transformers.models.roberta.modeling_roberta import * 
 
-class Net(nn.Module):
+class Net(pl.LightningModule):
   """Net [Roberta model for multilabel classification]
 
   This model uses two separate embeddings for both alpha and beta chain, followed by a Roberta backbone 
@@ -33,6 +39,40 @@ class Net(nn.Module):
     
     # Roberta classifier
     self.classifier = nn.Linear(4096, self.n_labels)
+    
+    # Metrics 
+    self.metrics = {
+      'train_accuracy': torchmetrics.Accuracy(),
+      'test_accuracy': torchmetrics.Accuracy(),
+      
+      'train_precision_micro': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'train_precision_macro': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'train_precision_samples': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='samples'),
+      'train_precision_all': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average=None),
+      
+      'test_precision_micro': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'test_precision_macro': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'test_precision_samples': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average='samples'),
+      'test_precision_all': torchmetrics.Precision(num_classes=self.n_labels, threshold=0.5, average=None),
+
+      'train_recall_micro': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'train_recall_macro': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'train_recall_samples': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='samples'),
+      'train_recall_all': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average=None),
+
+      'test_recall_micro': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'test_recall_macro': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'test_recall_samples': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average='samples'),
+      'test_recall_all': torchmetrics.Recall(num_classes=self.n_labels, threshold=0.5, average=None),
+
+      'train_F1_micro': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'train_F1_macro': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'train_F1_samples': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='samples'),
+      
+      'test_F1_micro': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='micro'),
+      'test_F1_macro': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='macro'),
+      'test_F1_samples': torchmetrics.F1Score(num_classes=self.n_labels, threshold=0.5, average='samples')
+    }
       
   def forward(self, input_ids_alpha:tensor, input_ids_beta:tensor) -> tensor:
       
@@ -60,3 +100,69 @@ class Net(nn.Module):
     output = torch.sigmoid(output)
     
     return output 
+  
+  def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=5e-3)
+        return optimizer
+  
+  def compute_step(self,batch):
+        ids_CDR3a, ids_CDR3b, target = batch["ids_CDR3a"], batch["ids_CDR3b"], batch['target']
+        label_logits = self.forward(ids_CDR3a, ids_CDR3b)
+        threshold = torch.tensor([0.5])
+        label_pred = (label_logits>threshold).float()*1
+        return self.loss(label_logits,target), target, label_logits, label_pred
+      
+  def log_metrics(self, type:str, loss:torch.Tensor, target:torch.Tensor, label_logits:torch.Tensor) -> None:
+    self.metrics[type+'_accuracy'](label_logits, target)
+    self.metrics[type+'_precision_micro'](label_logits, target)
+    self.metrics[type+'_precision_macro'](label_logits, target)
+    self.metrics[type+'_precision_samples'](label_logits, target)
+    
+    self.metrics[type+'_recall_micro'](label_logits, target)
+    self.metrics[type+'_recall_macro'](label_logits, target)
+    self.metrics[type+'_recall_samples'](label_logits, target)
+    
+    self.metrics[type+'_F1_micro'](label_logits, target)
+    self.metrics[type+'_F1_macro'](label_logits, target)
+    self.metrics[type+'_F1_samples'](label_logits, target)
+
+    self.logger.experiment.add_scalar({type+"/loss": loss, type+'/acc' : self.train_accuracy,
+                    type+"/precision_micro": self.metrics[type+'_precision_micro'], 
+                    type+"/precision_macro": self.metrics[type+'_precision_macro'],
+                    type+"/precision_samples": self.metrics[type+'_precision_samples'],
+                    type+"/recall_micro": self.metrics[type+'_recall_micro'], 
+                    type+"/recall_macro": self.metrics[type+'_recall_macro'],
+                    type+"/recall_samples": self.metrics[type+'_recall_samples'],
+                    type+"/F1_micro": self.metrics[type+'_F1_micro'], 
+                    type+"/F1_macro": self.metrics[type+'_F1_macro'],
+                    type+"/F1_samples": self.metrics[type+'_F1_samples']}, 
+              on_step=False, 
+              on_epoch=True, 
+              prog_bar=True)
+    
+    self.logger.experiment.add_scalar(type+'/precision_all',
+                                      {idx: val for idx, val in enumerate(self.metrics[type+'_precision_all'])},
+              on_step=False, 
+              on_epoch=True, 
+              prog_bar=True)
+    
+    self.logger.experiment.add_scalar(type+'/recall_all',
+                                      {idx: val for idx, val in enumerate(self.metrics[type+'_precision_all'])},
+              on_step=False, 
+              on_epoch=True, 
+              prog_bar=True)
+  
+  def training_step(self, train_batch):
+        loss, target, label_logits, _ = self.compute_step(train_batch)
+        self.log_metrics("train", loss, target, label_logits)
+        return loss
+
+  def test_step(self, val_batch):
+        loss, target, label_logits, _ = self.compute_step(val_batch)
+        self.log_metrics("val", loss, target, label_logits)
+        return loss
+      
+  def validation_step(self, val_batch):
+        loss, target, label_logits, _ = self.compute_step(val_batch)
+        self.log_metrics("val", loss, target, label_logits)
+        return loss  
